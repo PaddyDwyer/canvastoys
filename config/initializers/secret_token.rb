44f04b747ec114af5535c9d168d7f1c5fb209dbe
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2012-05-12 22:25:29 -0400

    Initial Commit
    Setting up the project using the default rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canvas::Application.config.secret_token = '7760dde28d971238831a3aec3a4cbd38179b5cf15d86308682a0f9ebb4068fa130670bde9c2c8abcba6facd1003bd0ddf6c03329738610d9e3ff97ed47c7b6f9'
