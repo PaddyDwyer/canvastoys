014c262fd146e0f9acf0db776a5e35feacca7b42
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2012-05-20 19:21:40 -0400

    Removing the database

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ gem 'rails', '3.2.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+# remove database
+#gem 'sqlite3'
 
 gem 'haml-rails'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# disable database
+#require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -44,11 +50,12 @@ module Canvas
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
+    # Disabling database
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    #config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
