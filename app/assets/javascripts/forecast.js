f3a3b052c13b54147176a56648498016171f955e
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2012-12-20 19:14:33 +0000

    Drawing the day seperators

--- app/assets/javascripts/forecast.js
+++ app/assets/javascripts/forecast.js
@@ -1,3 +1,14 @@
+var SurfForecastData = (function() {
+  SurfForecastData.name = "SurfForecastData";
+
+  function SurfForecastData(dataElement) {
+    this.day = parseInt(dataElement.getAttribute("day"), 0);
+    this.hour = dataElement.getAttribute("hour");
+  }
+
+  return SurfForecastData;
+})();
+
 var SurfForecast = (function() {
   SurfForecast.name = "SurfForecast";
 
@@ -6,19 +17,9 @@ var SurfForecast = (function() {
     this.stage = new Kinetic.Stage({container: "surf_forecast", width: 709, height: 166});
     this.backgroundLayer = new Kinetic.Layer();
     this.graphWidth = 689;
-    var rect = new Kinetic.Rect({
-        x: 19.5,
-        y: 23.5,
-        width: this.graphWidth,
-        height: 119,
-        stroke: "#969696",
-        strokeWidth: 1
-    });
-
-    this.backgroundLayer.add(rect);
+    this.graphOffsetX = 19.5;
 
     this.stage.add(this.backgroundLayer);
-
     this.fetchData();
   }
 
@@ -29,11 +30,61 @@ var SurfForecast = (function() {
   }
 
   SurfForecast.prototype.bindData = function(data, textStatus, xhr) {
-    this.data = data;
-    this.dataPoints = data.getElementsByTagName("data").length;
-    this.dataColumnWidth = this.graphWidth / this.dataPoints;
+    var dataPoints = data.getElementsByTagName("data");
+    var surfForecastDataArray = [];
+    var firstDataPoint;
+    for (var i = 0; i < dataPoints.length; i++) {
+      var dataPoint = new SurfForecastData(dataPoints[i]);
+      if (!firstDataPoint) {
+        firstDataPoint = dataPoint;
+      }
+      if (dataPoint.day == firstDataPoint.day + 7 && dataPoint.hour == firstDataPoint.hour ) {
+        break;
+      } else {
+        surfForecastDataArray.push(dataPoint);
+      }
+    }
+
+    this.data = surfForecastDataArray;
+    this.dataColumnWidth = this.graphWidth / this.data.length;
+    this.generateSurfBackground();
   }
 
+
+  SurfForecast.prototype.generateSurfBackground = function() {
+    // Build the surrounding rectangle
+    var rect = new Kinetic.Rect({
+        x: this.graphOffsetX,
+        y: 23.5,
+        width: this.graphWidth,
+        height: 119,
+        stroke: "#969696",
+        strokeWidth: 1
+    });
+    this.backgroundLayer.add(rect);
+
+
+    // Build the day seperators
+    var points, ytop = 22, ybottom = 142;
+    for (var i = 0; i < this.data.length; i++) {
+      if (this.data[i].hour === "12am") {
+        points = []
+        var x = Math.round(((i * this.dataColumnWidth) + this.graphOffsetX) + 0.5) - 0.5;
+        points.push(x);
+        points.push(ytop);
+        points.push(x);
+        points.push(ybottom);
+        var daySeperatorLine = new Kinetic.Line({
+          points: points,
+          stroke: '#d9d9d9',
+          strokeWidth: 1
+        });
+        this.backgroundLayer.add(daySeperatorLine);
+      }
+    }
+
+    this.backgroundLayer.draw();
+  }
 
   return SurfForecast;
 })();
